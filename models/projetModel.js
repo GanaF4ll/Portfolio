8355b10b1dfc9f3a09c3d266bbc016e010949974
GanaF4ll <ganafall9498@gmail.com>
2024-03-01 14:25:16 +0100

    Add fields to projetSchema in projetModel.js

--- models/projetModel.js
+++ models/projetModel.js
@@ -1,6 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-let projetSchema = new Schema({});
+let projetSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  completed: Boolean,
+});
 
 module.exports = mongoose.model("Projet", projetSchema);
