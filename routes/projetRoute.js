69204b6f9deca4a2de62dc6b0c6b077d90c277f6
GanaF4ll <ganafall9498@gmail.com>
2024-03-01 15:01:38 +0100

    Add routes for adding and listing projects

--- routes/projetRoute.js
+++ routes/projetRoute.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const projetController = require("../controllers/projetController");
 
-router.post("/", projetController.create_a_projet);
-router.get("/", projetController.list_all_projets);
+router.post("/addprojet", projetController.create_a_projet);
+router.get("/allprojet", projetController.list_all_projets);
 router.get("/:projetId", projetController.read_a_projet);
+
+module.exports = router;
