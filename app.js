2a0a2ca1e2ac957ecbc964794700126dc9e2bfec
GanaF4ll <ganafall9498@gmail.com>
2024-03-02 12:34:01 +0100

    Update routes and file paths

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/Portfolio");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/projets", require("./routes/projetRoute"));
-app.use("/infos", require("./routes/infoRoute"));
+app.use("/projets", require("./back-end/routes/projetRoute"));
+app.use("/infos", require("./back-end/routes/infoRoute"));
 
 app.listen(port, () => {
   console.log(`Portfolio app is running on port ${port}`);
